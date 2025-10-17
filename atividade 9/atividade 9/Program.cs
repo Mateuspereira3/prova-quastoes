ad5d9cfef1f69f1e1e40f4d759bfa917044af6a3
Mateuspereira3 <mateuspereira11a@gmail.com>
2025-10-17 17:24:26 -0300

    Add files via upload

---
+++ atividade 9/atividade 9/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] nomes = new string [5] {"mateus","adriana","diego","maria","madson"};
+string busca;
+
+Console.WriteLine("digite o nome da pessoa que deseja buscar");
+busca = Console.ReadLine();
+for (int i = 0; i <= nomes.Length; i++) {
+  
+    if (nomes[i] == busca)
+    {
+        Console.WriteLine($"o nome : {busca} foi encontrado , ele estava na casa {i} do vetor   ");
+        i = 1000000;
+    }
+   
+
+}; 
+
+Console.WriteLine($"o nome : {busca} foi encontrado , ele estava na casa {i} do vetor   ");
